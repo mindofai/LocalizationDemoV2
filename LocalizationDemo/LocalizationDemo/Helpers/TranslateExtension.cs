4379852d013162f76f498b3b4d4256b1b0fa0895
AMAIISDOM\bgarcia <BryanAnthony.Garcia@navitaire.com>
2017-07-07 14:21:00 +0800

    Initial w/o Platform specific

---
+++ LocalizationDemo/LocalizationDemo/Helpers/TranslateExtension.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LocalizationDemo.Helpers
+{
+    // You exclude the 'Extension' suffix when using in Xaml markup
+    [ContentProperty("Text")]
+    public class TranslateExtension : IMarkupExtension
+    {
+        readonly CultureInfo ci = null;
+        const string ResourceId = "LocalizationDemo.Resx.AppResources";
+
+        public TranslateExtension()
+        {
+            ci = CultureInfo.CurrentCulture;
+        }
+
+        public string Text { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null)
+                return "";
+
+            ResourceManager temp = new ResourceManager(ResourceId, typeof(TranslateExtension).GetTypeInfo().Assembly);
+
+            var translation = temp.GetString(Text, ci);
+            if (translation == null)
+            {
+                try
+                {
+                    translation = temp.GetString(Text, new CultureInfo("en"));
+                }
+                catch
+                {
+                    throw new ArgumentException(
+                        String.Format("Key '{0}' was not found in resources '{1}' for culture '{2}'.", Text, ResourceId, ci.Name),
+                        "Text");
+                }
+            }
+            return translation;
+        }
+    }
+}
--- LocalizationDemo/LocalizationDemo/MainPage.xaml.cs
+++ LocalizationDemo/LocalizationDemo/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LocalizationDemo.Resx;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ namespace LocalizationDemo
         public MainPage()
         {
             InitializeComponent();
+            //NameLabel.Text = AppResources.NameText;
+            NameEntry.Placeholder = AppResources.NamePlaceholder;
         }
     }
 }
